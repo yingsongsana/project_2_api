ddbcd64b2ef1be2b0cc1527d65087efbde3b0fe1
yingsongsana <yingsongsana@gmail.com>
2019-10-17 11:32:24 -0400

    Added curl script for wine index
    Index now works. Curl script for create is now fully functioning
    (issue: missed commas)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :wines, except: %i[new edit]
+  resources :wines# , except: %i[new edit]
   # RESTful routes
   resources :examples, except: %i[new edit]
 
--- curl-scripts/wines/create.sh
+++ curl-scripts/wines/create.sh
@@ -4,15 +4,15 @@ curl "http://localhost:4741/wines" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
-  # --header "Authorization: Token token=${TOKEN}" \
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "wine": {
-      "style": "'"${STYLE}"'"
-      "variety": "'"${VARIETY}"'"
-      "producer": "'"${PRODUCER}"'"
-      "appearance": "'"${APPEARANCE}"'"
-      "aroma": "'"${AROMA}"'"
-      "tasting_notes": "'"${NOTES}"'"
+      "style": "'"${STYLE}"'",
+      "variety": "'"${VARIETY}"'",
+      "producer": "'"${PRODUCER}"'",
+      "appearance": "'"${APPEARANCE}"'",
+      "aroma": "'"${AROMA}"'",
+      "tasting_notes": "'"${NOTES}"'",
       "memories": "'"${MEM}"'"
     }
   }'
